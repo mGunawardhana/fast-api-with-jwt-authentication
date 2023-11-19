7beb399ada6f887b73ff6220ff5962a59a6d6237
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-11-19 11:47:59 +0530

    completed

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,4 +1,6 @@
 import time
+from typing import Dict
+
 import jwt
 from decouple import config
 
@@ -10,16 +12,19 @@ def token_response(token: str):
     return {"access_token": token}
 
 
-def sign_jwt(user_id: str) -> str:
+def sign_jwt(user_id: str) -> dict[str, str]:
     payload = {"user_id": user_id, "expires": time.time() + 180}
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return token
+    return token_response(token)
 
 
 def decode_jwt(token: str) -> dict:
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         return decoded_token if decoded_token["expires"] >= time.time() else None
-
-    except:
+    except jwt.ExpiredSignatureError:
+        return {"error": "Expired token"}
+    except jwt.DecodeError:
         return {"error": "Invalid token"}
+    except Exception as e:
+        return {"error": f"An error occurred: {str(e)}"}
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Depends
 from app.model import PostSchema
 from app.model import PostSchema, UserSchema, UserLoginSchema
 from app.auth.jwt_handler import sign_jwt
-
+from app.auth.jwt_bearer import jwtBearer
 
 posts = [{"id": 1, "title": "penguins 🐧", "text": "Penguins are a group of aquatic flightless birds."},
-    {"id": 2, "title": "tigers 🐯",
-     "text": "Tigers are the largest living cat species and a members of the genus Panthers."},
-    {"id": 3, "title": "koalas 🐨", "text": "Koala is arboreal herbivorous marsupial native to Australia ."}, ]
+         {"id": 2, "title": "tigers 🐯",
+          "text": "Tigers are the largest living cat species and a members of the genus Panthers."},
+         {"id": 3, "title": "koalas 🐨", "text": "Koala is arboreal herbivorous marsupial native to Australia ."}, ]
 
 users = []
 
@@ -35,11 +35,28 @@ async def find_by_id(id: int):
             return {"data": post}
 
 
-@app.post("/posts", tags=["posts"])
+@app.post("/posts", dependencies=[Depends(jwtBearer())], tags=["posts"])
 async def save_post(post: PostSchema):
     post.id = len(posts) + 1
     posts.append(post.dict())
-    return {
-        "info": "Save Success!",
-        "data": post
-    }
+    return {"info": "Save Success!", "data": post}
+
+
+@app.post("/user/signup", tags=["user"])
+async def signup(user: UserSchema = Body(default=None)):
+    users.append(user)
+    return sign_jwt(user.email)
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+@app.post("/user/login", tags=["user"])
+async def login(user: UserLoginSchema = Body(default=None)):
+    if check_user(user):
+        return sign_jwt(user.email)
+    return {"error": "Wrong credentials"}
